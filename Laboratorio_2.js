7ccb653af700d411854980f16148ef3ce0f671da
Andres Aramendiz <adaramendiz@gmail.com>
2025-03-15 08:26:21 -0500

    first commit

---
+++ Laboratorio_2.js
@@ -0,0 +1,35 @@
+let num1, num2;
+let operacion;
+
+function realizarOperacion(num1, num2, operacion) {
+  if (operacion === "suma") {
+    return num1 + num2;
+  } else if (operacion === "resta") {
+    return num1 - num2;
+  } else if (operacion === "multiplicacion") {
+    return num1 * num2;
+  } else if (operacion === "division") {
+    if (num2 === 0) {
+      return "Error: No se puede dividir por cero.";
+    }
+    return num1 / num2;
+  } else {
+    return "Error: Operación no válida.";
+  }
+}
+
+while (true) {
+  operacion = prompt(
+    "Ingrese la operación a realizar (suma, resta, multiplicacion, division) o 'salir' para terminar:",
+  );
+  if (operacion.toLowerCase() === "salir") {
+    console.log("Saliendo del programa...");
+    break;
+  }
+
+  num1 = parseFloat(prompt("Ingrese el primer número:"));
+  num2 = parseFloat(prompt("Ingrese el segundo número:"));
+
+  let resultado = realizarOperacion(num1, num2, operacion);
+  console.log(`Resultado: ${resultado}`);
+}
